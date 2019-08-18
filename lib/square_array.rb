3c3b44649de9428a66b2f222643529a65e96e1e6
Matt Plichta <bigcatplichta@gmail.com>
2019-08-18 23:55:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   counter = 0 
-  new_array = []
    
     while array[counter] do
-      new_array >> array[counter] ** 2
+      array[counter] ** 2
       
       counter += 1 
     end  
