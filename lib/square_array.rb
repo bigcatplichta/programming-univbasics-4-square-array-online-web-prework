cef66487e1ba2045898461074279a793fcb27b47
Matt Plichta <bigcatplichta@gmail.com>
2019-08-18 23:58:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
    
     while array[counter] do
       new_array = array[counter] ** 2
-      puts new_array 
+      return new_array 
       
       counter += 1 
     end  
